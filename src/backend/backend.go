91639b6f577f19f92bdd59a73ccce4cd97e77546
m-faried <m.a.faried@gmail.com>
2024-04-14 01:23:56 +0200

    creating the order routes

--- src/backend/backend.go
+++ src/backend/backend.go
@@ -35,6 +35,11 @@ func (a *App) initializeRoutes() {
 	a.router.HandleFunc("/products", a.allProducts).Methods("GET")
 	a.router.HandleFunc("/products/{id}", a.fetchProduct).Methods("GET")
 	a.router.HandleFunc("/products", a.newProduct).Methods("POST")
+
+	a.router.HandleFunc("/orders", a.allOrders).Methods("GET")
+	a.router.HandleFunc("/orders/{id}", a.fetchOrder).Methods("GET")
+	a.router.HandleFunc("/orders", a.newOrder).Methods("POST")
+	a.router.HandleFunc("/orderitems", a.newOrderItem).Methods("POST")
 }
 
 func (a *App) Run() {
@@ -91,6 +96,79 @@ func (a *App) newProduct(res http.ResponseWriter, req *http.Request) {
 	}
 
 	respondWithJSON(res, http.StatusOK, p)
+}
+
+func (a *App) allOrders(res http.ResponseWriter, req *http.Request) {
+	orders, err := getAllOrders(a.database)
+	if err != nil {
+		fmt.Printf("allOrders err: %s\n", err.Error())
+		respondWithError(res, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(res, http.StatusOK, orders)
+}
+
+func (a *App) fetchOrder(res http.ResponseWriter, req *http.Request) {
+	// Parsing the submitted id.
+	vars := mux.Vars(req)
+	id := vars["id"]
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		fmt.Printf("fetchOrder invalid order ID: %s\n", err.Error())
+		respondWithError(res, http.StatusBadRequest, "Invalid Order ID")
+		return
+	}
+
+	// Reading the corresponding product from the database.
+	var o order
+	o.ID = intID
+	err = o.getOrder(a.database)
+	if err != nil {
+		fmt.Printf("fetchOrder err: %s\n", err.Error())
+		respondWithError(res, http.StatusNotFound, "Product ID Is Not Found")
+		return
+	}
+	respondWithJSON(res, http.StatusOK, o)
+}
+
+func (a *App) newOrder(res http.ResponseWriter, req *http.Request) {
+	reqBody, _ := io.ReadAll(req.Body)
+	var o order
+	json.Unmarshal(reqBody, &o)
+	err := o.createOrder(a.database)
+	if err != nil {
+		fmt.Printf("newOrder error: %s\n", err.Error())
+		respondWithError(res, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	for _, item := range o.Items {
+		item.OrderID = o.ID
+		err := item.createOrderItem(a.database)
+		if err != nil {
+			fmt.Printf("newOrder error: %s\n", err.Error())
+			respondWithError(res, http.StatusBadRequest, err.Error())
+			return
+		}
+	}
+
+	respondWithJSON(res, http.StatusOK, o)
+}
+
+func (a *App) newOrderItem(res http.ResponseWriter, req *http.Request) {
+	reqBody, _ := io.ReadAll(req.Body)
+	var items []orderItem
+	json.Unmarshal(reqBody, &items)
+	for _, item := range items {
+		err := item.createOrderItem(a.database)
+		if err != nil {
+			fmt.Printf("newOrderItem error: %s\n", err.Error())
+			respondWithError(res, http.StatusBadRequest, err.Error())
+			return
+		}
+	}
+
+	respondWithJSON(res, http.StatusOK, items)
 }
 
 //////////////////// Helper Functions

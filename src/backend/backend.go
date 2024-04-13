ea54d573adf891d0239a87966d4033f7a43c6e9a
m-faried <m.a.faried@gmail.com>
2024-04-13 22:07:46 +0200

    creating products routes

--- src/backend/backend.go
+++ src/backend/backend.go
@@ -2,7 +2,10 @@ package backend
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"strconv"
+
 	"log"
 	"net/http"
 
@@ -16,11 +19,6 @@ type App struct {
 	Port     string
 }
 
-func helloWorld(res http.ResponseWriter, req *http.Request) {
-	log.Println("A new request received!")
-	fmt.Fprintf(res, "Hello World")
-}
-
 func (a *App) Initialize() {
 	database, err := sql.Open("sqlite3", "../../practiceit.db")
 	if err != nil {
@@ -32,7 +30,9 @@ func (a *App) Initialize() {
 }
 
 func (a *App) initializeRoutes() {
-	a.router.HandleFunc("/", helloWorld).Methods("GET")
+	a.router.HandleFunc("/", healthCheck).Methods("GET")
+	a.router.HandleFunc("/products", a.allProducts).Methods("GET")
+	a.router.HandleFunc("/products/{id}", a.fetchProduct).Methods("GET")
 }
 
 func (a *App) Run() {
@@ -40,4 +40,59 @@ func (a *App) Run() {
 	// http.Handle("/", a.router)
 	fmt.Println("Server started and listening on the port", a.Port)
 	log.Fatal(http.ListenAndServe(a.Port, a.router))
+}
+
+func (a *App) allProducts(res http.ResponseWriter, req *http.Request) {
+	products, err := getProducts(a.database)
+	if err != nil {
+		fmt.Printf("getProducts err: %s\n", err.Error())
+		respondWithError(res, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJSON(res, http.StatusOK, products)
+}
+
+func (a *App) fetchProduct(res http.ResponseWriter, req *http.Request) {
+
+	// Parsing the submitted id.
+	vars := mux.Vars(req)
+	id := vars["id"]
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		fmt.Printf("fetchProduct invalid product ID: %s\n", err.Error())
+		respondWithError(res, http.StatusBadRequest, "Invalid Product ID")
+		return
+	}
+
+	// Reading the corresponding product from the database.
+	var prod product
+	prod.ID = intID
+	err2 := prod.getProduct(a.database)
+	if err2 != nil {
+		fmt.Printf("fetchProduct err: %s\n", err2.Error())
+		respondWithError(res, http.StatusNotFound, "Product ID Is Not Found")
+		return
+	}
+	respondWithJSON(res, http.StatusOK, prod)
+}
+
+//////////////////// Helper Functions
+
+func healthCheck(res http.ResponseWriter, req *http.Request) {
+	log.Println("A new request received!")
+	fmt.Fprintf(res, "Hello World")
+}
+
+// respondWithError sets the error response to the response writer
+func respondWithError(res http.ResponseWriter, code int, message string) {
+	respondWithJSON(res, code, map[string]string{"error": message})
+}
+
+// respondWithJSON sets the response payload in the response writer
+func respondWithJSON(res http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(code)
+	res.Write(response)
 }

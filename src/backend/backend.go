3b0989be0fff15c91bdc03b5948a8afa154ca0e3
m-faried <m.a.faried@gmail.com>
2024-04-13 17:05:03 +0200

    updating the backend to become struct based

--- src/backend/backend.go
+++ src/backend/backend.go
@@ -1,17 +1,34 @@
 package backend
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type App struct {
+	db   *sql.DB
+	Port string
+}
+
 func helloWorld(res http.ResponseWriter, req *http.Request) {
+	log.Println("A new request received!")
 	fmt.Fprintf(res, "Hello World")
 }
 
-func Run(addr string) {
+func (a *App) Initialize() {
+	database, err := sql.Open("sqlite3", "../../practiceit.db")
+	a.db = database
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func (a *App) Run() {
 	http.HandleFunc("/", helloWorld)
-	fmt.Println("Server started and listening on the port", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	fmt.Println("Server started and listening on the port", a.Port)
+	log.Fatal(http.ListenAndServe(a.Port, nil))
 }
--- src/backend/go.mod
+++ src/backend/go.mod
@@ -1,3 +1,5 @@
 module mofaried/backend
 
 go 1.22.2
+
+require github.com/mattn/go-sqlite3 v1.14.22

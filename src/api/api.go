fdf9796813b4509a59a399d961e6d3a7a733a8aa
m-faried <m.a.faried@gmail.com>
2024-04-23 06:41:48 +0200

    implementing init and running of the the grpc

--- src/api/api.go
+++ src/api/api.go
@@ -3,19 +3,24 @@ package api
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	ctrls "github.com/m-faried/api/controllers"
 	models "github.com/m-faried/api/models"
 	httpRouters "github.com/m-faried/api/presentations/routers"
+	rpc "github.com/m-faried/api/presentations/rpc"
+	"google.golang.org/grpc"
 )
 
 type App struct {
-	Port     string
-	DBPath   string
-	database *models.EcommerceDal
-	router   *mux.Router
+	Port       string
+	PortGrpc   string
+	DBPath     string
+	database   *models.EcommerceDal
+	router     *mux.Router
+	grpcServer *rpc.GrpcEcommerceServer
 }
 
 func (a *App) Initialize() {
@@ -31,13 +36,29 @@ func (a *App) Initialize() {
 
 	// Creating & initializaing the router
 	a.initHttpRouter()
+
+	// Initializing Grpc
+	a.initGrpc()
 }
 
-func (a *App) Run() {
+func (a *App) RunHttp() {
 	// You can do the following as well.
 	// http.Handle("/", a.router)
 	fmt.Println("Server started and listening on the port", a.Port)
 	log.Fatal(http.ListenAndServe(a.Port, a.router))
+}
+
+func (a *App) RunGrpc() {
+	lis, err := net.Listen("tcp", a.PortGrpc)
+	if err != nil {
+		log.Fatalf("Cannot create listener: %s", err)
+	} else {
+		fmt.Println("gRPC started and listending on the port", a.PortGrpc)
+	}
+	serverRegistrar := grpc.NewServer()
+	rpc.RegisterECommerceServer(serverRegistrar, a.grpcServer)
+	err = serverRegistrar.Serve(lis)
+	fmt.Println("The error", err)
 }
 
 func (a *App) initHttpRouter() {
@@ -55,6 +76,13 @@ func (a *App) initHttpRouter() {
 	or.InitRoutes()
 
 	a.router = router
+}
+
+func (a *App) initGrpc() {
+	pc := ctrls.NewProductsController(a.database)
+	oc := ctrls.NewOrdersController(a.database)
+	ps := rpc.NewGrpcEcommerceServer(pc, oc)
+	a.grpcServer = ps
 }
 
 //////////////////// Helper Functions

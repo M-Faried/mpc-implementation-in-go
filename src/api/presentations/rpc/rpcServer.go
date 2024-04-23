5dc08547aecc9fd0864f145ed0aebd18c9112898
m-faried <m.a.faried@gmail.com>
2024-04-23 14:25:50 +0200

    implemented get single product method

--- src/api/presentations/rpc/rpcServer.go
+++ src/api/presentations/rpc/rpcServer.go
@@ -11,6 +11,7 @@ import (
 // It contains the methods used by the GrpcEcommerceServer
 type IProductsController interface {
 	GetProducts() ([]models.Product, error)
+	GetSingleProduct(int) (*models.Product, error)
 }
 
 // IOrdersController is the requried interface for the order controller
@@ -35,6 +36,7 @@ func NewGrpcEcommerceServer(prodCtrl IProductsController, ordersCtrl IOrdersCont
 
 func (s *GrpcEcommerceServer) GetProducts(context.Context, *GetProductsRequest) (*GetProductsResponse, error) {
 
+	// Reading products data from the database
 	products, err := s.prodCtrl.GetProducts()
 	if err != nil {
 		fmt.Printf("GetAllProducts err: %s\n", err.Error())
@@ -43,18 +45,44 @@ func (s *GrpcEcommerceServer) GetProducts(context.Context, *GetProductsRequest) 
 
 	// Converting models.Product to rpc.Product
 	var rpcProducts []*Product
+	var currRpcProd *Product
 	for _, prod := range products {
-		rpcProducts = append(rpcProducts, &Product{
-			ID:          int32(prod.ID),
-			Price:       int32(prod.Price),
-			Inventory:   int32(prod.Inventory),
-			Name:        prod.Name,
-			ProductCode: prod.ProductCode,
-			Status:      prod.Status,
-		})
+		currRpcProd = getRpcProductFromModelProduct(&prod)
+		rpcProducts = append(rpcProducts, currRpcProd)
 	}
 
+	// Returning the result
 	return &GetProductsResponse{
 		Products: rpcProducts,
 	}, nil
+}
+
+func (s *GrpcEcommerceServer) GetSingleProduct(c context.Context, req *GetSignleProductRequest) (*GetSingleProductResponse, error) {
+
+	// Searching for the product into the database
+	prodID := int(req.ProductID)
+	p, err := s.prodCtrl.GetSingleProduct(prodID)
+	if err != nil {
+		fmt.Printf("GetSingleProduct err: %s\n", err.Error())
+		return nil, err
+	}
+
+	// Converting models.Product to rpc.Product
+	prod := getRpcProductFromModelProduct(p)
+
+	// Returning the result
+	return &GetSingleProductResponse{
+		Product: prod,
+	}, nil
+}
+
+func getRpcProductFromModelProduct(mProd *models.Product) *Product {
+	return &Product{
+		ID:          int32(mProd.ID),
+		Price:       int32(mProd.Price),
+		Inventory:   int32(mProd.Inventory),
+		Name:        mProd.Name,
+		ProductCode: mProd.ProductCode,
+		Status:      mProd.Status,
+	}
 }

4043d786ee9feffd25d308671e7422bd7a388275
m-faried <m.a.faried@gmail.com>
2024-04-23 05:45:02 +0200

    renaming the rpc server file

---
+++ src/api/presentations/rpc/rpcServer.go
@@ -0,0 +1,53 @@
+package rpc
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/m-faried/api/models"
+)
+
+type IProductsController interface {
+	GetProducts() ([]models.Product, error)
+}
+
+type IOrdersController interface{}
+
+type GrpcEcommerceServer struct {
+	UnimplementedECommerceServer
+	prodCtrl  IProductsController
+	orderCtrl IOrdersController
+}
+
+func NewGrpcEcommerceServer(prodCtrl IProductsController, ordersCtrl IOrdersController) *GrpcEcommerceServer {
+	return &GrpcEcommerceServer{
+		prodCtrl:  prodCtrl,
+		orderCtrl: ordersCtrl,
+	}
+}
+
+func (s *GrpcEcommerceServer) GetProducts(context.Context, *GetProductsRequest) (*GetProductsResponse, error) {
+
+	products, err := s.prodCtrl.GetProducts()
+	if err != nil {
+		fmt.Printf("GetAllProducts err: %s\n", err.Error())
+		return nil, err
+	}
+
+	rpcProducts := make([]*Product, len(products))
+
+	for _, prod := range products {
+		rpcProducts = append(rpcProducts, &Product{
+			ID:          int32(prod.ID),
+			Price:       int32(prod.Price),
+			Inventory:   int32(prod.Inventory),
+			Name:        prod.Name,
+			ProductCode: prod.ProductCode,
+			Status:      prod.Status,
+		})
+	}
+
+	return &GetProductsResponse{
+		Products: rpcProducts,
+	}, nil
+}
